891374f380931ecfa98c9b728a5fe5d9a06a9d67
Andrew MacGillivray <Andrew.MacGillivray@defence.gov.au>
2025-01-28 12:21:25 +1030

    Day 3 B

--- day3/3.cpp
+++ day3/3.cpp
@@ -10,25 +10,37 @@ using namespace std;
 int main() {
     // initialise variables
     string line;
-    const regex exp("mul\\([0-9]+\\,[0-9]+\\)");
+    const regex exp_A("mul\\([0-9]+\\,[0-9]+\\)");
+    const regex exp_B("mul\\([0-9]+\\,[0-9]+\\)|do\\(\\)|don\\'t\\(\\)");
     smatch result;
     int total = 0;
+    bool do_mul = true;
 
     // read input from file
     ifstream file("3_input.txt");
     while (getline (file, line)) {
         // perform iterative regex search for expected pattern
         string::const_iterator searchStart(line.cbegin());
-        while (regex_search(searchStart, line.cend(), result, exp)) {
+        while (regex_search(searchStart, line.cend(), result, exp_B)) { // A or B matcher
             string match = result.str(0);
-            // extract numbers for multiplication
-            string numbers = match.substr(4, match.length() - 5);
-            vector<string> numbers_list = split_string(numbers, ",");
-            total += (stoi(numbers_list[0]) * stoi(numbers_list[1]));
+            if (match == "do()") {
+                do_mul = true;
+            } else if (match == "don't()") {
+                do_mul = false;
+            } else {
+                // extract numbers for multiplication
+                string numbers = match.substr(4, match.length() - 5);
+                vector<string> numbers_list = split_string(numbers, ",");
+                // check if do is enabled, and if so add to total // PART B
+                if (do_mul) {
+                    total += (stoi(numbers_list[0]) * stoi(numbers_list[1]));
+                }
+            }
             // cout << (searchStart == line.begin() ? "" : " ") << result1[0];
             searchStart = result.suffix().first;
         }
     }
+    file.close();
 
     cout << "Total: " << total << endl;
     cout << endl;
