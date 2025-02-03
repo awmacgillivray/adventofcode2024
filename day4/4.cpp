80c03b4ce6185fca8d0776ce5f0a77193bc255a0
Andrew MacGillivray <awmacgillivray@gmail.com>
2025-02-03 14:35:48 +1030

    Day 4 B

--- day4/4.cpp
+++ day4/4.cpp
@@ -12,7 +12,7 @@ vector<vector<int>> find_neighbours(vector<vector<char>> table, vector<int> star
     {
         for (int offset2 : {-1, 0, 1})
         {
-            if (offset1 == 0 and offset2 == 0)
+            if (offset1 == 0 and offset2 == 0) // ignore own cell
             {
                 continue;
             }
@@ -33,6 +33,7 @@ vector<vector<int>> find_neighbours(vector<vector<char>> table, vector<int> star
             }
         }
     }
+    // return vector of index matches
     return match_offsets;
 }
 
@@ -40,7 +41,8 @@ int main()
 {
     // initialise variables
     vector<vector<char>> word_search;
-    int total_matches = 0;
+    int total_xmas_matches = 0;
+    int total_mas_matches = 0;
     string line;
 
     // load data from file
@@ -55,11 +57,13 @@ int main()
         word_search.push_back(row);
     }
 
-    // find occurences of 'X' and search from there
-    for (int i = 0; i < word_search.size(); i++)
+    int i_size = word_search.size();
+    for (int i = 0; i < i_size; i++)
     {
-        for (int j = 0; j < word_search[i].size(); j++)
+        int j_size = word_search[i].size();
+        for (int j = 0; j < j_size; j++)
         {
+            // find occurences of 'X' and search from there - PART A
             if (word_search[i][j] == 'X')
             {
                 // find location of any neighbouring 'M' characters
@@ -78,30 +82,65 @@ int main()
                     int index2 = j + offset2 * 2;
                     if (
                         (0 <= index1) &&
-                        (index1 < word_search.size()) &&
+                        (index1 < i_size) &&
                         (0 <= index2) &&
-                        (index2 < word_search[i].size()) &&
-                        ((word_search[index1][index2]) == 'A'))
+                        (index2 < j_size) &&
+                        (word_search[index1][index2] == 'A'))
                     {
                         index1 = i + offset1 * 3;
                         index2 = j + offset2 * 3;
                         if (
                             (0 <= index1) &&
-                            (index1 < word_search.size()) &&
+                            (index1 < i_size) &&
                             (0 <= index2) &&
-                            (index2 < word_search[i].size()) &&
+                            (index2 < j_size) &&
                             (word_search[index1][index2] == 'S'))
                         {
-                            total_matches++;
+                            total_xmas_matches++;
                         }
                     }
+                }
+            }
+            // find occurences of 'A' and search from there - PART B
+            if (word_search[i][j] == 'A')
+            {
+                // find location of any neighbouring 'M' characters
+                vector<vector<int>> matches_M = find_neighbours(word_search, vector<int>{i, j}, 'M');
+                // track occurences of 'M-A-S' matches
+                int num_diag_matches = 0;
+                for (int m = 0; m < matches_M.size(); m++)
+                {
+                    vector<int> match_M = matches_M[m];
+                    int offset1 = match_M[0];
+                    int offset2 = match_M[1];
+                    if ((offset1 == 0) || (offset2 == 0)) // ignore 'M' matches on the vertical/horizontal
+                    {
+                        continue;
+                    }
+                    int index1 = i - offset1;
+                    int index2 = j - offset2;
+                    // check bounds and if character opposite 'M' is an 'S'
+                    if (
+                        (0 <= index1) &&
+                        (index1 < i_size) &&
+                        (0 <= index2) &&
+                        (index2 < j_size) &&
+                        (word_search[index1][index2] == 'S'))
+                    {
+                        num_diag_matches++;
+                    }
+                }
+                if (num_diag_matches == 2) // must be exactly 2 diagonal 'M-A-S' matches
+                {
+                    total_mas_matches++;
                 }
             }
         }
     }
 
     // result
-    cout << "Total matches: " << total_matches << endl;
+    cout << "Total XMAS matches: " << total_xmas_matches << endl;
+    cout << "Total X-MAS matches: " << total_mas_matches << endl;
 
     cout << endl;
     return 0;
