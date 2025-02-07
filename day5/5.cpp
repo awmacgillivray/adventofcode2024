ed75ea5b518e16f4227ed842b7c0d6f53017b9b5
Andrew MacGillivray <awmacgillivray@gmail.com>
2025-02-07 12:26:32 +1030

    Day 5 separate into functions

--- day5/5.cpp
+++ day5/5.cpp
@@ -8,6 +8,66 @@
 
 using namespace std;
 
+
+vector<map<int, set<int>>> add_pages_to_transition_maps(
+    int page1,
+    int page2,
+    map<int, set<int>> fwd_transition_map,
+    map<int, set<int>> bwd_transition_map
+){
+    if (!fwd_transition_map.contains(page1))
+    {
+        fwd_transition_map[page1] = set<int>{page2};
+    }
+    else
+    {
+        fwd_transition_map.at(page1).insert(page2);
+    }
+    if (!bwd_transition_map.contains(page2))
+    {
+        bwd_transition_map[page2] = set<int>{page1};
+    }
+    else
+    {
+        bwd_transition_map.at(page2).insert(page1);
+    }
+    return {fwd_transition_map, bwd_transition_map};
+}
+
+
+bool check_update_order(
+    vector<int> update_pages_int,
+    map<int, set<int>> fwd_transition_map,
+    map<int, set<int>> bwd_transition_map
+){
+    for (int i = 0; i < update_pages_int.size(); i++)
+    {
+        int page = update_pages_int[i];
+        // check previous pages for correct order
+        for (int j = 0; j < i; j++)
+        {
+            int bwd_page = update_pages_int[j];
+            if (bwd_transition_map.contains(page) &&
+                !bwd_transition_map.at(page).contains(bwd_page))
+            {
+                return false;
+            }
+        }
+        // check subsequent pages for correct order
+        for (int j = i + 1; j < update_pages_int.size(); j++)
+        {
+            int fwd_page = update_pages_int[j];
+            if (fwd_transition_map.contains(page) &&
+                !fwd_transition_map.at(page).contains(fwd_page))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
 int main()
 {
     // initialise variables
@@ -17,76 +77,52 @@ int main()
     map<int, set<int>> fwd_transition_map;
     map<int, set<int>> bwd_transition_map;
     int total = 0;
+    int total_fixed = 0;
 
     // load data from file
     ifstream file("5_input.txt");
     while (getline(file, line))
     {
-        // build transition maps of page printnig order
+        // build transition maps of page printing order
         if (regex_match(line, exp_transition))
         {
             int page1 = stoi(line.substr(0, 2));
             int page2 = stoi(line.substr(3, 2));
-            if (!fwd_transition_map.contains(page1))
-            {
-                fwd_transition_map[page1] = set<int>{page2};
-            }
-            else
-            {
-                fwd_transition_map.at(page1).insert(page2);
-            }
-            if (!bwd_transition_map.contains(page2))
-            {
-                bwd_transition_map[page2] = set<int>{page1};
-            }
-            else
-            {
-                bwd_transition_map.at(page2).insert(page1);
-            }
+            vector<map<int, set<int>>> maps = add_pages_to_transition_maps(
+                page1, page2,
+                fwd_transition_map,
+                bwd_transition_map
+            );
+            fwd_transition_map = maps[0];
+            bwd_transition_map = maps[1];
         }
         // check update sequence for correct order
         else if (regex_match(line, exp_update))
         {
-            bool valid_update = true;
             vector<string> update_pages = split_string(line, ",");
-            for (int i = 0; i < update_pages.size(); i++)
+            vector<int> update_pages_int;
+            for (string page : update_pages)
             {
-                int page = stoi(update_pages[i]);
-                for (int j = 0; j < i; j++)
-                {
-                    int bwd_page = stoi(update_pages[j]);
-                    if (bwd_transition_map.contains(page) &&
-                        !bwd_transition_map.at(page).contains(bwd_page))
-                    {
-                        valid_update = false;
-                        break;
-                    }
-                }
-                for (int j = i + 1; j < update_pages.size(); j++)
-                {
-                    int fwd_page = stoi(update_pages[j]);
-                    if (fwd_transition_map.contains(page) &&
-                        !fwd_transition_map.at(page).contains(fwd_page))
-                    {
-                        valid_update = false;
-                        break;
-                    }
-                }
-                if (!valid_update)
-                {
-                    break;
-                }
+                update_pages_int.push_back(stoi(page));
             }
-            if (valid_update)
+            // part A
+            if (check_update_order(update_pages_int, fwd_transition_map, bwd_transition_map))
             {
-                total += stoi(update_pages[update_pages.size() / 2]);
-                cout << line << endl;
+                total += update_pages_int[update_pages_int.size() / 2];
+                // cout << line << endl;
+            }
+            // part B
+            else
+            {
+                // find potential valid order and new total
+                ;
             }
         }
     }
 
     // result
-    cout << "Total: " << total << endl;
+    cout << endl << "Total: " << total << endl;
+    cout << endl << "Total fixed: " << total_fixed << endl;
     cout << endl;
     return 0;
 }
